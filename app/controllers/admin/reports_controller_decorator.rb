da1aff8631822c932a2de08ee473088860f8824d
Torsten Rüger <torsten@lightning.nu>
2011-11-25 20:18:46 +0200

    some searches work

--- app/controllers/admin/reports_controller_decorator.rb
+++ app/controllers/admin/reports_controller_decorator.rb
@@ -10,24 +10,25 @@ Admin::ReportsController.class_eval do
   def simple
     search = params[:search] || {}
     search[:meta_sort] = "created_at.asc"
-    if search[:order_created_at_greater_than].blank?
-      search[:order_created_at_greater_than] = Time.now - 3.months
+    if search[:created_at_greater_than].blank?
+      search[:created_at_greater_than] = Time.now - 3.months
     else
-      search[:order_created_at_greater_than] = Time.zone.parse(search[:order_created_at_greater_than]).beginning_of_day rescue Time.zone.now.beginning_of_month
+      search[:created_at_greater_than] = Time.zone.parse(search[:created_at_greater_than]).beginning_of_day rescue Time.zone.now.beginning_of_month
     end
-    unless search[:order_created_at_less_than].blank?
-      search[:order_created_at_less_than] =
-          Time.zone.parse(search[:order_created_at_less_than]).end_of_day rescue ""
+    unless search[:created_at_less_than].blank?
+      search[:created_at_less_than] =
+          Time.zone.parse(search[:created_at_less_than]).end_of_day rescue search[:created_at_less_than]
     end
-
+    period = params[:period] || "week"
+    price_or = (params[:price_or] || "amount").to_sym
     search[:order_completed_at_is_not_null] = true
 #:barWidth => 24*60*60*1000
     @search = LineItem.metasearch(search)
-    data = @search.all.collect {|i| [i.created_at.to_i * 1000 , i.price * i.quantity ] }
+    data = @search.all.collect { |i|  [i.created_at.to_i * 1000 , i.send(price_or) ] }
     @flot_options = { :series => {  :lines =>  { :show => true , :fill => true , :steps => true } } ,
                       :xaxis =>  { :mode => "time" }  
                     }
-    @flot_data = [{ :label => :all , :data => bucket_array( data , :week )}]
+    @flot_data = [{ :label => :all , :data => bucket_array( data , period )}]
     
 # csv ?      send_data( render_to_string( :csv , :layout => false) , :type => "application/csv" , :filename => "tilaukset.csv") 
   render :template => "admin/reports/simple" 
