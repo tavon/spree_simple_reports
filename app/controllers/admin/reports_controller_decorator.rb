449407f1ef9d01b0d2a97b0b9e606385c0db3a5d
Torsten Rüger <torsten@lightning.nu>
2011-11-29 12:34:15 +0200

    redid bucketing to get stacking to work

--- app/controllers/admin/reports_controller_decorator.rb
+++ app/controllers/admin/reports_controller_decorator.rb
@@ -20,13 +20,13 @@ Admin::ReportsController.class_eval do
           Time.zone.parse(search[:created_at_less_than]).end_of_day rescue search[:created_at_less_than]
     end
     @period = params[:period] || "week"
-    days = 1
-    days = 7 if @period == "week"
-    days = 30.5 if @period == "month"
+    @days = 1
+    @days = 7 if @period == "week"
+    @days = 30.5 if @period == "month"
     @price_or = (params[:price_or] || "total").to_sym
     search[:order_completed_at_is_not_null] = true
     @search = LineItem.metasearch(search)
-    @flot_options = { :series => {  :bars =>  { :show => true , :barWidth => days * 24*60*60*1000 } , :stack => 1 } , 
+    @flot_options = { :series => {  :bars =>  { :show => true , :barWidth => @days * 24*60*60*1000 } , :stack => 0 } , 
                       :legend => {  :container => "#legend"} , 
                       :xaxis =>  { :mode => "time" }  
                     }
@@ -37,9 +37,12 @@ Admin::ReportsController.class_eval do
   
   def group_data
     @group_by = (params[:group_by] ||:all).to_sym
+    all = @search.all
     flot = {}
+    smallest = all.first.created_at
+    largest = all.last.created_at
     if( @group_by == :all )
-      flot[:all] = @search.all 
+      flot[:all] = all
     else
       @search.all.each do |item|
         bucket = item.send( @group_by )
@@ -47,65 +50,48 @@ Admin::ReportsController.class_eval do
         flot[ bucket ] << item        
       end
     end
-    @flot_data = flot.collect do |label , data | 
-      chart_data = data.collect {|i| [i.created_at.to_i*1000 , i.send(@price_or)] } ;
-      { :label => label.label , :data => bucket_array( chart_data , @period ) } 
+    @flot_data = flot.collect do |label , data |
+      { :label => label.label , :data => bucket_array( data , smallest , largest ) } 
     end
   end
 
-  # the array is assumed to contain [ js-times (to_i*1000) , number ] arrays
   # a new bucketet array version is returned 
-  def bucket_array( array , by)
-    ret = [ array.first.dup ]
-    array.each_with_index do |value , index |
-      next if index == 0
-      at , val = value
-      last = ret.last
-      if( Time.at(at/1000).send(by) == Time.at(last[0]/1000).send(by) )
-        last[1] = last[1] + val
-      else
-        ret << value.dup
-      end
+  # a value is creted for every tick between from and two (so all arrays have same length)
+  # ticks int he returned array are javascsript times ie milliseconds since 1970
+  def bucket_array( array  , from , to )
+    rb_tick = (@days * 24 * 60 * 60).to_i
+    js_tick = rb_tick * 1000
+    from = (from.to_i / rb_tick) * js_tick
+    to = (to.to_i / rb_tick)* js_tick
+    ret = {}
+    while from <= to
+      ret[from] = 0
+      from += js_tick
     end
-    ret
+    array.each do |item|
+      value = item.send(@price_or)
+      index = (item.created_at.to_i / rb_tick)*js_tick
+      throw ret.to_json unless ret[index]
+      ret[index] = ret[index] + value
+    end
+    ret.sort
   end
   
 end
 
-Symbol.class_eval do
-  def label
-    self
-  end
-end
-String.class_eval do
-  def label
-    self
-  end
-end
-Taxon.class_eval do
-  def label
-    self.name
-  end
-end
-Product.class_eval do
-  def label
-    self.name
-  end
-end
-Variant.class_eval do 
-  def label
-    self.full_name
-  end
-end
 LineItem.class_eval do
   def by_taxon
-    self.variant.product.taxons.first || "none"
+    if self.variant.product.taxons.first 
+      self.variant.product.taxons.first.name
+    else
+      "none"
+    end
   end
   def by_product
-    self.variant.product
+    self.variant.product.name
   end
   def by_variant
-    self.variant
+    self.variant.full_name
   end
 end
 
